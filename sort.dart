fb4acf30b43980fde215dd59f4af4e00b8d3493e
Arshiyan Ali <155827717+Arshiyan-ali@users.noreply.github.com>
2024-07-18 05:09:00 +0500

    Update sort.dart

--- sort.dart
+++ sort.dart
@@ -2,9 +2,9 @@ void main(){
 
   
   List num=[5,4,3,2,1];
-  for(int i=0;i<num.length;i++){
-    for(int j=0;j<num.length;j++){
-      if (num[i]<num[j]) {
+  for(int i=0;i<num.length-1;i++){
+    for(int j=i+1;j<num.length;j++){
+      if (num[i]>num[j]) {
         int rem=num[i];
         num[i]=num[j];
         num[j]=rem;
